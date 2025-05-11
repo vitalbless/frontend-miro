d1f553b5584d762e41484a3eee1a5e49b5c546bb
Ivan Smetanin <ivan.smetanin100@gmail.com>
2025-05-11 10:55:05 +0300

    настроил tailwind + установил shacdcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import viteTsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths()],
+  plugins: [react(), viteTsconfigPaths(), tailwindcss()],
 });
